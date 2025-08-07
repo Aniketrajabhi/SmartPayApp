c12544f746574b4bd584783dd4feceb42330bd4e
Aniketrajabhi <aniketrajabhi123@gmail.com>
2025-08-07 20:38:34 +0530

    final setups in yaml file and docker configurations

--- auth-service/src/main/java/org/example/config/SecurityConfig.java
+++ auth-service/src/main/java/org/example/config/SecurityConfig.java
@@ -4,15 +4,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.example.JwtKeyUtil;
 
+@EnableWebFluxSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeExchange(ex -> ex.anyExchange().authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -39,4 +52,11 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+//    @Bean
+//    InMemoryUserDetailsManager userss() {
+//        PasswordEncoder enc = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("admin").password(enc.encode("admin")).roles("USER").build());
+//    }
 }
